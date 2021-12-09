f38e255f237a78bbdaf9b15290ba8f0adf29eebe
Paulo Henrique Silva <pshenrique2015@gmail.com>
2021-12-09 11:40:07 -0300

    The game is working

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 //it's totally free to use
 //Github: Paulo-Henrique-Silva
 
-//in this version, the user can make one guess and the hanged man's draws are finished, but not working
+//in this version the game works, but there's a bug that the game does not finish when the body is complete
+//and the word always reset
 
 #include <stdio.h>
 #include <conio.h>
@@ -14,7 +15,7 @@ FILE *pWords; //this will contain the words
 
 char words[][35] = {"HANGMAN GAME"}; //this is temporary
 
-void refreshHanged();
+void refreshHanged(int wrongGuesses);
 void refreshWord(char letter);
 char isValid_letter(char letter); 
 int isThere_theLetter(char letter);
@@ -23,96 +24,100 @@ int isThere_theLetter(char letter);
 int main()
 {
     char letter = ' ';
-    //refreshHanged();
-    refreshWord(letter); 
+    int wrongGuesses = 0;
 
-    printf("\n\nType a Letter: ");
-    scanf(" %c", &letter); 
+    while(wrongGuesses <= 6)
+    {
+        refreshHanged(wrongGuesses);
+        refreshWord(letter); 
 
-    letter = isValid_letter(letter); 
+        printf("\n\nType a Letter: ");
+        scanf(" %c", &letter); 
+        letter = isValid_letter(letter); 
 
-    if(isThere_theLetter(letter))
-        printf("Right guess!\n\n");
-    else
-        printf("Wrong Guess :/\n\n");
+        if(isThere_theLetter(letter))
+            printf("Right guess! :)");
+        else
+        {
+            printf("Wrong Guess :/");
+            wrongGuesses++;
+        }
 
-    refreshWord(letter); 
+        getch();
+    } 
 
-    getch(); //stops before closes it
+   // getch(); //stops before closes it
     return 0;
 }
 
-void refreshHanged()
+void refreshHanged(int wrongGuesses)
 {
-    for(int i = 0; i < 7; i++)
+    switch(wrongGuesses)
     {
-        switch(i)
-        {
-            case 0:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|         ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 1:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 2:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|        |  ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 3:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|       /|  ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 4:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|       /|\\");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 5:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|       /|\\");
-                printf("\n\t\t\t\t\t|       /   ");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-            case 6:
-                printf("\n\t\t\t\t\t|--------|");
-                printf("\n\t\t\t\t\t|        O");
-                printf("\n\t\t\t\t\t|       /|\\");
-                printf("\n\t\t\t\t\t|       / \\");
-                printf("\n\t\t\t\t\t|           ");
-                printf("\n\t\t\t\t\t|__________");
-                break;
-        }
-        getch();
+        case 0:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|         ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 1:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 2:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|        |  ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 3:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|       /|  ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 4:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|       /|\\");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 5:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|       /|\\");
+            printf("\n\t\t\t\t\t|       /   ");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
+        case 6:
+            printf("\n\t\t\t\t\t|--------|");
+            printf("\n\t\t\t\t\t|        O");
+            printf("\n\t\t\t\t\t|       /|\\");
+            printf("\n\t\t\t\t\t|       / \\");
+            printf("\n\t\t\t\t\t|           ");
+            printf("\n\t\t\t\t\t|__________");
+            break;
     }
 }
-//this will refresh for each user right guess
+//this will refresh for each user wrong guess
 
 void refreshWord(char letter)
 {
     int i;
+    printf("\n\n\t\t");
 
     for(i = 0; i < strlen(words[0]); i++)
     {
